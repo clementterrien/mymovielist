779235f87d8d592ebb12d1a157001ec5a95221d5
Clément Terrien <clement.terrien@hotmail.fr>
2021-03-04 16:29:27 +0100

    import orm-fixtures

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -5,6 +5,7 @@ namespace App\Tests;
 use PHPUnit\Framework\TestCase;
 
 class AppTest extends TestCase {
+    
     public function testTestsAreWorking(){
         $this->assertEquals(2, 1+1);
     }
